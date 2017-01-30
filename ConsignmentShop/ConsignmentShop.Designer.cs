e5ec5e23a926ae459eafb2c41c8ce59a198ab730
Sonia Kurtz <kurtzsonia@gmail.com>
2017-01-30 19:20:30 +0100

    Start of a new project

--- ConsignmentShop/ConsignmentShop.Designer.cs
+++ ConsignmentShop/ConsignmentShop.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.headerText = new System.Windows.Forms.Label();
+            this.itemsListBox = new System.Windows.Forms.ListBox();
+            this.itemsListBoxLabel = new System.Windows.Forms.Label();
+            this.addToCart = new System.Windows.Forms.Button();
+            this.shoppingCartLabel = new System.Windows.Forms.Label();
+            this.shoppingCartListBox = new System.Windows.Forms.ListBox();
+            this.makePurchase = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // headerText
@@ -41,11 +47,77 @@
             this.headerText.TabIndex = 0;
             this.headerText.Text = "Consignment Shop Demo";
             // 
+            // itemsListBox
+            // 
+            this.itemsListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.itemsListBox.FormattingEnabled = true;
+            this.itemsListBox.ItemHeight = 20;
+            this.itemsListBox.Location = new System.Drawing.Point(12, 105);
+            this.itemsListBox.Name = "itemsListBox";
+            this.itemsListBox.Size = new System.Drawing.Size(337, 204);
+            this.itemsListBox.TabIndex = 1;
+            // 
+            // itemsListBoxLabel
+            // 
+            this.itemsListBoxLabel.AutoSize = true;
+            this.itemsListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.itemsListBoxLabel.Location = new System.Drawing.Point(8, 82);
+            this.itemsListBoxLabel.Name = "itemsListBoxLabel";
+            this.itemsListBoxLabel.Size = new System.Drawing.Size(101, 20);
+            this.itemsListBoxLabel.TabIndex = 2;
+            this.itemsListBoxLabel.Text = "Store items";
+            // 
+            // addToCart
+            // 
+            this.addToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.addToCart.Location = new System.Drawing.Point(355, 180);
+            this.addToCart.Name = "addToCart";
+            this.addToCart.Size = new System.Drawing.Size(157, 41);
+            this.addToCart.TabIndex = 3;
+            this.addToCart.Text = "Add to Cart ->";
+            this.addToCart.UseVisualStyleBackColor = true;
+            // 
+            // shoppingCartLabel
+            // 
+            this.shoppingCartLabel.AutoSize = true;
+            this.shoppingCartLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.shoppingCartLabel.Location = new System.Drawing.Point(514, 82);
+            this.shoppingCartLabel.Name = "shoppingCartLabel";
+            this.shoppingCartLabel.Size = new System.Drawing.Size(124, 20);
+            this.shoppingCartLabel.TabIndex = 5;
+            this.shoppingCartLabel.Text = "Shopping Cart";
+            // 
+            // shoppingCartListBox
+            // 
+            this.shoppingCartListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.shoppingCartListBox.FormattingEnabled = true;
+            this.shoppingCartListBox.ItemHeight = 20;
+            this.shoppingCartListBox.Location = new System.Drawing.Point(518, 105);
+            this.shoppingCartListBox.Name = "shoppingCartListBox";
+            this.shoppingCartListBox.Size = new System.Drawing.Size(337, 204);
+            this.shoppingCartListBox.TabIndex = 4;
+            // 
+            // makePurchase
+            // 
+            this.makePurchase.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.makePurchase.Location = new System.Drawing.Point(698, 315);
+            this.makePurchase.Name = "makePurchase";
+            this.makePurchase.Size = new System.Drawing.Size(157, 37);
+            this.makePurchase.TabIndex = 6;
+            this.makePurchase.Text = "Purchase";
+            this.makePurchase.UseVisualStyleBackColor = true;
+            // 
             // ConsignmentShop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1362, 741);
+            this.ClientSize = new System.Drawing.Size(867, 427);
+            this.Controls.Add(this.makePurchase);
+            this.Controls.Add(this.shoppingCartLabel);
+            this.Controls.Add(this.shoppingCartListBox);
+            this.Controls.Add(this.addToCart);
+            this.Controls.Add(this.itemsListBoxLabel);
+            this.Controls.Add(this.itemsListBox);
             this.Controls.Add(this.headerText);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.Margin = new System.Windows.Forms.Padding(9, 9, 9, 9);
@@ -60,6 +132,12 @@
         #endregion
 
         private System.Windows.Forms.Label headerText;
+        private System.Windows.Forms.ListBox itemsListBox;
+        private System.Windows.Forms.Label itemsListBoxLabel;
+        private System.Windows.Forms.Button addToCart;
+        private System.Windows.Forms.Label shoppingCartLabel;
+        private System.Windows.Forms.ListBox shoppingCartListBox;
+        private System.Windows.Forms.Button makePurchase;
     }
 }
 
