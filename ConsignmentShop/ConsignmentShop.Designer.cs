7ae4cee45be6260080e2f5b6535fe6bab9723e4b
Sonia Kurtz <kurtzsonia@gmail.com>
2017-01-30 18:59:15 +0100

    Start of a new project

--- ConsignmentShop/ConsignmentShop.Designer.cs
+++ ConsignmentShop/ConsignmentShop.Designer.cs
@@ -51,6 +51,7 @@
             this.Margin = new System.Windows.Forms.Padding(9, 9, 9, 9);
             this.Name = "ConsignmentShop";
             this.Text = "ConsignmentShopDemo";
+            this.Load += new System.EventHandler(this.ConsignmentShop_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ConsignmentShop/ConsignmentShop.cs
+++ ConsignmentShop/ConsignmentShop.cs
@@ -16,5 +16,10 @@ namespace ConsignmentShop
         {
             InitializeComponent();
         }
+
+        private void ConsignmentShop_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
